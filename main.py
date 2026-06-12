5f05cbfbdefc453d79b77b430252950174164a88
Andrew Guest <andrew@aguest.me>
2026-06-11 20:07:34 -0500

    Lower profile samping rate

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ sentry_sdk.init(
     traces_sample_rate=1.0,
     # Set profile_session_sample_rate to 1.0 to profile 100%
     # of profile sessions.
-    profile_session_sample_rate=1.0,
+    profile_session_sample_rate=0.1,
     # Set profile_lifecycle to "trace" to automatically
     # run the profiler on when there is an active transaction
     profile_lifecycle="trace",
